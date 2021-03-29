557f7b2966525b23a0a299972c732a6c163bef26
surfmuggle <1271555+surfmuggle@users.noreply.github.com>
2021-03-29 17:20:24 +0200

    all ac are working, eding status should be improved, clean up missing

--- todoMDN.js
+++ todoMDN.js
@@ -1,4 +1,4 @@
-var dragged;
+var dragged ;
 let app = {
     draggedElement: null,
     todos: []
@@ -14,8 +14,6 @@ function fillTodoList()
     const newListForm = document.querySelector('[data-new-list-form]')
     console.log("newListForm", newListForm);
 }
-
-
 
 function renderTodos(targetNode, todos)
 {
@@ -53,10 +51,6 @@ function getTodos()
 
 // #endregion
 
-function todoDragEndEvent(event)
-{
-
-}
 
 function onDragStart(event)
 {
@@ -75,7 +69,7 @@ function onDragEnter(event)
 {
     // highlight potential drop target when the draggable element enters it
     if ( event.target.className == "dropzone" ) {
-        event.target.style.background = "purple";
+        event.target.style.background = "#fae6fa ";
     }
 }
 
@@ -91,8 +85,10 @@ function onDrop(event) {
     console.log("onDrop draggedElement, event", app.draggedElement, event);
     // prevent default action (open as link for some elements)
     event.preventDefault();
+    console.log("event.target.className", event.target.className);
     console.log("onDrop event.target.parentNode", event.target.parentNode);
-
+    let targetList = document.elementFromPoint(event.clientX, event.clientY);
+    console.log("onDrop elementFromPoint", targetList);
     // move dragged elem to the selected drop target
     if ( event.target.className == "dropzone" || event.target.parentNode.className == "dropzone"  ) {
         event.target.style.background = "";
@@ -104,12 +100,19 @@ function onDrop(event) {
         else {
             event.target.appendChild( app.draggedElement );
         }
-        // setTimeout(()=>{console.log("timeout")}, 300);
+        let todoIndex = app.todos.findIndex(todo => todo.id == app.draggedElement.getAttribute("data-id"));
+        let newStatus = targetList.getAttribute("data-todo-status");
+        app.todos[todoIndex].status = newStatus;
+        app.draggedElement.setAttribute("class", newStatus );
+        console.log("app.todos[todoIndex]",app.todos[todoIndex]); 
     }
 }
 
 function addEventListener()
 {
+
+    document.getElementById("addTodo").addEventListener('keyup', onTodoKeyUp, false);
+    
     var dropNodes = document.querySelectorAll(".dropzone");
 
     dropNodes.forEach(node => {
@@ -124,6 +127,23 @@ function addEventListener()
     });
 }
 
+
+
+function onTodoKeyUp(event)
+{
+    if(event.key === "Enter")
+    {
+        let newId = app.todos.length +1;
+        let todo = {id: newId, title: event.target.value, status: "open"};
+        // this only adds an todo but does not render it 
+        app.todos.push(todo);
+        renderTodos(document.getElementById("todoList"), app.todos);
+        // clear input text
+        event.target.value = "";
+
+    }
+    console.log("onTodoKeyUp event  ",event);
+}
 
 
 function init()
